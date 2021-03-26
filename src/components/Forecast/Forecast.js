46cb204d190ccc8338047d282f47301ac7e87813
nandhini093 <nandhini9320@gmail.com>
2021-03-26 18:53:12 +0530

    26th march 18:53

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -20,7 +20,7 @@ const Forecast = () => {
     setLoading(true);
     /*let uriEncodedCity = encodeURIComponent(city);*/
 
-    fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=Seattle&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html", {
+    fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=seattle&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "bd63ea38a1msh9ee293126e03f6cp192d21jsn78d498056c51",
